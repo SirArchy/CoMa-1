efadfbf622e8e9c527ac54e1113917999316e28b
SirArchy <wnbemail123@googlemail.com>
2023-02-18 09:43:09 +0100

    final version

--- CoMa_Proggen_HA13.py
+++ CoMa_Proggen_HA13.py
@@ -1,17 +1,17 @@
-class Vertex:
+class Vertex: #✔
     def __init__(self, name):
         self.name = name
         self.neighbours = []
 
     def __str__(self):
-        return f"Vertex {self.name} with neighbours {self.neighbours}"
+        return f"Vertex {self.name} with neighbours {self.neighbours}."
 
     def add_neighbour(self, neighbour):
         if isinstance(neighbour, Vertex):
             self.neighbours.append(neighbour.name)
             neighbour.neighbours.append(self.name)
 
-class Graph:
+class Graph: #✔
     def __init__(self):
         self.num_vertices = 0
         self.num_edges = 0
@@ -30,6 +30,8 @@ class Graph:
                 self.num_vertices += 1
 
     def add_edge(self, vertex_name1, vertex_name2):
+        if vertex_name1 == vertex_name2:
+            return
         if vertex_name1 not in self.vertices:
             self.add_vertices([vertex_name1])
         if vertex_name2 not in self.vertices:
@@ -52,104 +54,82 @@ class Graph:
 
 
 
-class Bipartite_Graph(Graph):
+class Bipartite_Graph(Graph): #✔
     def __init__(self):
         super().__init__()
-        self.verticesA = []
-        self.verticesB = []
+        self.partition_A = []
+        self.partition_B = []
 
-    def __str__(self):
-        num_verticesA = len(self.verticesA)
-        num_verticesB = len(self.verticesB)
-        num_edges = self.num_edges
-        return f'Bipartite graph with {num_verticesA} vertices in A, {num_verticesB} vertices in B and {num_edges} edges.'
+    def __str__(self): #✔
+        num_partition_A = len(self.partition_A)
+        num_partition_B = len(self.partition_B)     
+        return f"Bipartite graph with {num_partition_A} vertices in A, {num_partition_B} vertices in B and {self.num_edges} edges."
 
-    def add_vertices(self, vertex_names, partition):
+    def add_vertices(self, vertex_names, partition = None): #✔
         for name in vertex_names:
-            if name in self.vertices:
-                continue
-            vertex = Vertex(name)
-            self.vertices[name] = vertex
             if partition == 'A':
-                self.verticesA.append(name)
+                if name not in self.partition_A:
+                    self.partition_A.append(name)                   
             elif partition == 'B':
-                self.verticesB.append(name)
+                if name not in self.partition_B:
+                    self.partition_B.append(name)
+        super().add_vertices(vertex_names)
 
     def add_edge(self, vertex_nameA, vertex_nameB):
-        if vertex_nameA in self.verticesB or vertex_nameB in self.verticesA:
-            return
+        if vertex_nameA not in self.partition_A and vertex_nameA not in self.partition_B:
+            self.partition_A.append(vertex_nameA)
+        if vertex_nameB not in self.partition_B and vertex_nameB not in self.partition_A:
+            self.partition_B.append(vertex_nameB)
+        self.add_vertices([vertex_nameA])
+        self.add_vertices([vertex_nameB])
+
+        if vertex_nameA in self.partition_B or vertex_nameB in self.partition_A:                   
+            return #if its the case then it will return nothing
+
         super().add_edge(vertex_nameA, vertex_nameB)
-
-
-"""   
-u = Vertex('u')
-v = Vertex('v')
-w = Vertex('w')
-u.add_neighbour(v)
-u.add_neighbour(w)
-print(u)
-#Vertex u with neighbours ['v', 'w']
-print(v)
-#Vertex v with neighbours ['u']
-"""
-
-G = Graph()
-G.add_vertices(['u','v','w'])
-G.add_edge('u','v')
-G.add_edge('u','w')
-print(G)
-#Graph with 3 vertices and 2 edges.
-G.add_edge('u','x')
-print(G)
-#Graph with 3 vertices and 2 edges.
-G.add_edge('x','y')
-print(G)
-#Graph with 3 vertices and 2 edges.
-G.add_edge('z','a')
-print(G)
-#Graph with 3 vertices and 2 edges.
-
-for i in G:
-    print(i)
-#Vertex u with neighbours ['v', 'w'].
-#Vertex v with neighbours ['u'].
-#Vertex w with neighbours ['u'].
-G.get_max_degree()
-#2
 
 """
 B = Bipartite_Graph()
-B.add_vertices(['a1','a2'],'A')
-B.add_vertices(['b1'],'B')
-B.add_edge('a1','b1')
-B.add_edge('a2','b1')
+B.add_vertices(['a0', 'a1', 'a2', 'a3', 'a4'],"A")
+B.add_vertices(['b0', 'b1', 'b2', 'b3', 'b4', 'b5'],"B")
+B.add_edge("a4","b5")
+B.add_edge("a3","b4")
+B.add_edge("a2","b3")
+B.add_edge("a3","a3")
+B.add_edge("a0","a1")
 print(B)
-#Bipartite graph with 2 vertices in A, 1 vertices in B and 2 edges.
-B.add_edge('a1','a2')
-print(B)
-#Bipartite graph with 2 vertices in A, 2 vertices in B and 2 edges.
-B.add_edge('a1','a2')
-print(B)
-#Bipartite graph with 2 vertices in A, 2 vertices in B and 2 edges.
+#Erwarteter Output: Bipartite graph with 5 vertices in A, 6 vertices in B and 3 edges.
+for i in B: print(i)
+#Vertex a0 with neighbours [].
+#Vertex a1 with neighbours [].
+#Vertex a2 with neighbours ['b3'].
+#Vertex a3 with neighbours ['b4'].
+#Vertex a4 with neighbours ['b5'].
+#Vertex b0 with neighbours [].
+#Vertex b1 with neighbours [].
+#Vertex b2 with neighbours [].
+#Vertex b3 with neighbours ['a2'].
+#Vertex b4 with neighbours ['a3'].
+#Vertex b5 with neighbours ['a4'].
 print(B.get_max_degree())
-#2
+#1
 
 
-C = Bipartite_Graph()
-C.add_vertices(['a0', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6'],"A")
-C.add_edge("a4","b3")
-C.add_edge("a6","b0")
-C.add_edge("a1","b0")
-C.add_edge("a2","b0")
-C.add_edge("a5","b0")
-C.add_edge("a2","b2")
-C.add_edge("a6","b3")
-C.add_edge("a1","b2")
-C.add_edge("a5","b5")
-C.add_edge("a6","b5")
-print(C)
-# Bipartite graph with 7 vertices in A, 4 vertices in B and 10 edges.
-for i in C: print(i)
+B = Bipartite_Graph()
+B.add_vertices(['a0', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6'],"A")
+B.add_edge("a4","b3")
+B.add_edge("a6","b0")
+B.add_edge("a1","b0")
+B.add_edge("a2","b0")
+B.add_edge("a5","b0")
+B.add_edge("a2","b2")
+B.add_edge("a6","b3")
+B.add_edge("a1","b2")
+B.add_edge("a5","b5")
+B.add_edge("a6","b5")
+print(B)
+#Bipartite graph with 7 vertices in A, 4 vertices in B and 10 edges.
+for i in B: print(i)
 #Vertex a0 with neighbours [].
 #Vertex a1 with neighbours ['b0', 'b2'].
 #Vertex a2 with neighbours ['b0', 'b2'].
@@ -161,6 +141,6 @@ for i in C: print(i)
 #Vertex b0 with neighbours ['a6', 'a1', 'a2', 'a5'].
 #Vertex b2 with neighbours ['a2', 'a1'].
 #Vertex b5 with neighbours ['a5', 'a6'].
-C.get_max_degree()
+B.get_max_degree()
 #4
 """
